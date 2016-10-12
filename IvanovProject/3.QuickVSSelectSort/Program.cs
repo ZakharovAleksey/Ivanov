50259c8835c606b51486e45fb2bcd4362e94c522
ZakharovAleksey <lehazakharov@hotmail.com>
2016-10-12 14:12:46 +0300

    Length function implementation
    TODO: ID function implementation

--- IvanovProject/3.QuickVSSelectSort/Program.cs
+++ IvanovProject/3.QuickVSSelectSort/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -111,27 +112,107 @@ namespace _3.QuickVSSelectSort
         #region  Solver depending on ID
 
 
+        void PrepareLeftPart(MyArray array, ref Stack<MyArray> stack)
+        {
+            for (int curStep = 1; curStep < array.Size; ++curStep)
+            {
+                MyArray tempArray = (MyArray)array.Clone();
+                tempArray.BubbleSortStep(curStep);
+                stack.Push((MyArray)tempArray.Clone());
+            }
+        }
+
+        void SolverID(int lenght)
+        {
+            MyArray array = new MyArray(lenght);
+
+            MyArray rightSort = (MyArray)array.Clone();
+            Stack<MyArray> stack = new Stack<MyArray>();
+
+
+
+        }
+
         #endregion
 
 
         #region Solver depending on Length
 
-        void SolverLenght(MyArray array)
+        public static void SolverLenght(int minLenght, int maxLenght, int step)
         {
-            // hello
-   
-            //lockal
+            MyInt.AssigmentCount = 0;
+            MyInt.CompareCount = 0;
+
+            #region StreamWriter prepare
+
+            StreamWriter qSortSWAssigment = new StreamWriter(@"QSortLenghtAssigment.txt");
+            StreamWriter qSortSWCompare = new StreamWriter(@"QSortLenghtCompare.txt");
+
+            StreamWriter mergeSortSWAssigment = new StreamWriter(@"MergeSortLenghtAssigment.txt");
+            StreamWriter mergeSortSWCompare = new StreamWriter(@"MergeSortLenghtCompare.txt");
+
+            #endregion
+
+            for (int curLenght = minLenght; curLenght <= maxLenght; curLenght += step)
+            {
+                #region Initilize arays
+
+                MyArray qSortArray = new MyArray(curLenght);
+                qSortArray.fillWithRandomValues(0, 10);
+
+                MyArray mergeSortArray = (MyArray)qSortArray.Clone();
+
+                #endregion
+
+                #region QSort
+
+                QuickSort(ref qSortArray, 0, qSortArray.Size - 1);
+
+                Console.WriteLine("QSORT: compare = {0}, assigment = {1}", MyInt.CompareCount, MyInt.AssigmentCount);
+                qSortSWAssigment.WriteLine("{0} {1}", qSortArray.Size, MyInt.AssigmentCount);
+                qSortSWCompare.WriteLine("{0} {1}", qSortArray.Size, MyInt.CompareCount);
+
+                MyInt.AssigmentCount = 0;
+                MyInt.CompareCount = 0;
+
+                #endregion
+
+                #region MergeSort
+
+                MergeSort(ref mergeSortArray, 0, mergeSortArray.Size - 1);
+
+                Console.WriteLine("MERGESORT: compare = {0}, assigment = {1}", MyInt.CompareCount, MyInt.AssigmentCount);
+                mergeSortSWAssigment.WriteLine("{0} {1}", mergeSortArray.Size, MyInt.AssigmentCount);
+                mergeSortSWCompare.WriteLine("{0} {1}", mergeSortArray.Size, MyInt.CompareCount);
+
+                MyInt.AssigmentCount = 0;
+                MyInt.CompareCount = 0;
+
+                #endregion
+            }
+
+            #region StreamWriter close
+
+            qSortSWAssigment.Close();
+            qSortSWCompare.Close();
+
+            mergeSortSWAssigment.Close();
+            mergeSortSWCompare.Close();
+
+            #endregion
         }
 
         #endregion
 
         static void Main(string[] args)
         {
-            MyArray a = new MyArray(8);
-            a.fillWithRandomValues(0, 100);
-            Console.WriteLine(a);
-            QuickSort(ref a, 0, a.Size - 1);
-            Console.WriteLine(a);
+            //MyArray a = new MyArray(8);
+            //a.fillWithRandomValues(0, 100);
+            //Console.WriteLine(a);
+            //QuickSort(ref a, 0, a.Size - 1);
+            //Console.WriteLine(a);
+
+            SolverLenght(10, 100, 10);
 
 
             //MyArray a = new MyArray(8);
