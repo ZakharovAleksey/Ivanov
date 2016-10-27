8f181e5be32cf3d1ae8921c1ad23822411ade7ae
Aleksey <lehazakharov@yandex.ru>
2016-10-28 00:39:50 +0300

    My Linked List Implementation
    Main idea implementation

---
+++ IvanovProject/5.FrequencyDictionary/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5.FrequencyDictionary
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+            MyLinkedList<string> list = new MyLinkedList<string>();
+            list.Add("1");
+            list.Add("2");
+            list.Add("3");
+            list.Add("4");
+            list.Add("2");
+            list.Add("1");
+            list.Insert("10", 1);
+
+            list.Display();
+            Console.WriteLine(list.Count());
+
+
+    }
+  }
+}
