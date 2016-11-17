b793ff8ff1ae6e17f74769a3a2e7d94c816b80af
ZakharovAleksey <lehazakharov@hotmail.com>
2016-11-17 19:07:54 +0300

    Cell update
    - Cell class implementation, chessboard class implementation.
    - Chessboard graphics implamantation.

--- IvanovProject/6.BypassingTheChessboard/Form1.cs
+++ IvanovProject/6.BypassingTheChessboard/Form1.cs
@@ -8,13 +8,15 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace _6.BypassingTheChessboard
 {
     public partial class Form1 : Form
     {
 
         Bitmap drawingSurface;
-        Graphics gr;
+        Graphics graphic;
+
 
         public Form1()
         {
@@ -25,23 +27,43 @@ namespace _6.BypassingTheChessboard
         private void btnStart_Click(object sender, EventArgs e)
         {
             drawingSurface = new Bitmap(drawingArea.Width, drawingArea.Height);
-            gr = Graphics.FromImage(drawingSurface);
+            graphic = Graphics.FromImage(drawingSurface);
 
+            int boardSize = int.Parse(textBoxChessBoardSize.Text);
+            int cellSize = drawingArea.Width / boardSize;
 
-
-            gr.DrawRectangle(Pens.Black, 10, 10, 10, 10);
-            
-
-
+            ChessBoard board = new ChessBoard(boardSize, cellSize);
+            board.CalcInitalPossibleSteps();
+            DrawChessboard(graphic, board);
 
             drawingArea.Image = drawingSurface;
         }
 
-        #region ChessBoard
+        #region Drawing Functions
 
-        private static void DrawChessBoard(int size)
+        private static void DrawCell(Graphics graphic, Cell cell)
         {
-            int cellSize = 10;
+            switch (cell.Type)
+            {
+                case (int)CellType.BLACK:
+                    graphic.FillRectangle(Brushes.Black, new RectangleF(cell.X, cell.Y, cell.Width, cell.Height));
+                    break;
+                case (int)CellType.WHITE:
+                    graphic.FillRectangle(Brushes.White, new RectangleF(cell.X, cell.Y, cell.Width, cell.Height));
+                    break;
+                case (int)CellType.USED:
+                    graphic.FillRectangle(Brushes.Red, new RectangleF(cell.X, cell.Y, cell.Width, cell.Height));
+                    break;
+                default:
+                    return;
+            }
+        }
+
+        private static void DrawChessboard(Graphics graphic, ChessBoard board)
+        {
+            for (int row = 0; row < board.ChessBoardSize; ++row)
+                for (int col = 0; col < board.ChessBoardSize; ++col)
+                    DrawCell(graphic, board.at(row, col));
         }
 
         #endregion
